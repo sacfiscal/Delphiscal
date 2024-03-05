7b9f06abef622348385d96d4a30bd471b3cd1b09
rtcustodio <80653605+rtcustodio@users.noreply.github.com>
2024-03-05 08:45:44 -0300

    calculo FCP no ICMS CST00
    calculo FCP no ICMS CST00

---
+++ Delphiscal.Icms00.Intf.pas
@@ -0,0 +1,15 @@
+unit Delphiscal.Icms00.Intf;
+
+interface
+
+type
+  IIcms00 = interface
+    [ '{1562944F-E256-4BBB-9A9D-6DF253CE5150}' ]
+    function BaseIcmsProprio: Double;
+    function ValorIcmsProprio: Double;
+    function ValorFCP : Double;
+  end;
+
+implementation
+
+end.
---
+++ Delphiscal.Icms00.pas
@@ -0,0 +1,66 @@
+unit Delphiscal.Icms00;
+
+interface
+
+uses
+  Delphiscal.Icms.BaseProprio,
+  Delphiscal.Icms.Valor,
+  Delphiscal.Icms00.Intf;
+
+type
+  TIcms00 = class(TInterfacedObject,
+                  IIcms00)
+  private
+    FBaseIcmsProprio: TBaseIcmsProprio;
+    FValorIcms      : TValorIcms;
+    FValorFCP      : TValorIcms;
+    function BaseIcmsProprio: Double;
+    function ValorIcmsProprio: Double;
+    function ValorFCP : Double;
+  public
+    class function New(const AValorProduto, AValorFrete, AValorSeguro, ADespesasAcessorias, AValorDesconto, AALiquotaICMS: Double; const AValorIpi: Double = 0; const AAliquotaFCP : Double = 0): IIcms00;
+    constructor Create(const AValorProduto, AValorFrete, AValorSeguro, ADespesasAcessorias, AValorDesconto, AALiquotaICMS: Double; const AValorIpi: Double = 0; const AAliquotaFCP : Double = 0);
+    destructor Destroy; override;
+  end;
+
+implementation
+
+uses
+  Delphiscal.Utils;
+
+class function TIcms00.New(const AValorProduto, AValorFrete, AValorSeguro, ADespesasAcessorias, AValorDesconto, AALiquotaICMS, AValorIpi, AAliquotaFCP : Double): IIcms00;
+begin
+  Result := TIcms00.Create(AValorProduto, AValorFrete, AValorSeguro, ADespesasAcessorias, AValorDesconto, AALiquotaICMS, AValorIpi, AAliquotaFCP);
+end;
+
+constructor TIcms00.Create(const AValorProduto, AValorFrete, AValorSeguro, ADespesasAcessorias, AValorDesconto, AALiquotaICMS, AValorIpi, AAliquotaFCP: double);
+begin
+  FBaseIcmsProprio := TBaseIcmsProprio.Create(AValorProduto, AValorFrete, AValorSeguro, ADespesasAcessorias, AValorDesconto, 0, AValorIpi);
+  FValorIcms       := TValorIcms.Create(FBaseIcmsProprio, AALiquotaICMS);
+  FValorFCP        := TValorIcms.Create(FBaseIcmsProprio, AAliquotaFCP);
+end;
+
+destructor TIcms00.Destroy;
+begin
+  FValorIcms.Free;
+  FBaseIcmsProprio.Free;
+  FValorFCP.Free;
+  inherited;
+end;
+
+function TIcms00.BaseIcmsProprio: Double;
+begin
+  Result := RoundABNT(FBaseIcmsProprio.CalcularBaseIcmsProprio, 2);
+end;
+
+function TIcms00.ValorFCP: Double;
+begin
+  Result := RoundABNT(FValorFCP.GetValorIcms, 2);
+end;
+
+function TIcms00.ValorIcmsProprio: Double;
+begin
+  Result := RoundABNT(FValorIcms.GetValorIcms, 2);
+end;
+
+end.
